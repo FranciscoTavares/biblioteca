c99820650e850b9d1f4215d0d5c32e92c53ed936
franciscotavares <fco.anjos@gmail.com>
2017-06-16 13:45:24 -0300

    inicialização do projeto

---
+++ sources/main.c
@@ -0,0 +1,35 @@
+/*
+ * library project
+ * Descrição
+ *
+ * O acervo da biblioteca precisa ser informatizado. Para tanto, as principais informações
+ * das obras (livros) foram assim estruturas:
+ * Obra(numero de tombo, numero de exemplar, data compra)
+ * Tompo (numero de tombo, nome da obra, nome do autor, nome da editora, código da área)
+ *
+ * sabe-se que existem 20 tombos e, para cada um, existem, no máximo, três exemplares.
+ * defina vetores de registros para armazenar tais informações.
+ *
+ * Defina um menu de oções a seguir:
+ *
+ * cadastrar tombos;
+ * cadastrar obras;
+ * mostrar obras por áreas;
+ * mostrar obras por autor;
+ * mostrar obras por editora;
+ * encerrar programa.
+ *
+ *  Created on: Jun 16, 2017
+ *      Author: ftavares
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char** argv){
+
+	printf("hello!!");
+	return (EXIT_SUCCESS);
+}
+
+
